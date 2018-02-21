0e3a4d49894e4495b8b29bfd35aed5ae7466c0e4
Alec Doran-Twyford <alec@alectronic.co>
2018-02-21 14:49:58 +0000

    renaming ok build setup for cnp

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let http = require('http');
 let app = express();
 app.use(express.static('dist'));
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3621';
 const server = http.createServer(app);
 
 app.set('port', port);
