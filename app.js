203022026b76437d2028de1b70979eb2c0cf01e7
Louis Houghton <louis@louishoughton.com>
2018-02-23 10:58:14 +0000

    Add health end point (#31)
    * Add health end point
    
    * Fix codacy issues

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express');
-let http = require('http');
+const express = require("express");
+let http = require("http");
 let app = express();
-app.use(express.static('dist'));
+app.use("/summary", express.static(__dirname + "/dist"));
+app.get("/health", (req, res) => {
+  res.send({
+    status: "UP"
+  });
+});
+app.use(express.static("dist"));
 
-const port = process.env.PORT || '3621';
+
+const port = process.env.PORT || "3621";
 const server = http.createServer(app);
 
-app.set('port', port);
+app.set("port", port);
 
 server.listen(port);
