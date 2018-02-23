9a080dc27798be0cebde8d6e4aa921294bf2eddf
Louis Houghton <louis@louishoughton.com>
2018-02-23 10:16:41 +0000

    Add health end point

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 let http = require('http');
 let app = express();
+app.use('/summary', express.static(__dirname + '/dist'));
+app.get('/health', (req, res) => {
+  res.send({
+    status: "UP"
+  })
+});
 app.use(express.static('dist'));
+
 
 const port = process.env.PORT || '3621';
 const server = http.createServer(app);
