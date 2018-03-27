1be403e47c1de1b72f4a6dcb5af4b1d2fe732961
Louis Houghton <louis@louishoughton.com>
2018-03-27 14:16:03 +0100

    Run tests with a test app to serve pdf data

--- bin/test/test_unit.sh
+++ bin/test/test_unit.sh
@@ -1,31 +1,10 @@
 #!/bin/sh
-yarn test:coverage
 
-xdg-open report/units.html
-open report/units.html
+# Kill & Run app
+pkill -f "node test-app"
+nohup node test-app &
 
-xdg-open coverage/index.html
-open coverage/index.html
+yarn run-tests
 
-#./gradlew check
-#./gradlew jacocoTestReport --info
-#./gradlew sonarqube -Dsonar.host.url=$SONARQUBE_URL
-#
-#xdg-open build/reports/checkstyle/main.html
-#open build/reports/checkstyle/main.html
-#
-#xdg-open build/reports/checkstyle/test.html
-#open build/reports/checkstyle/test.html
-#
-#xdg-open build/reports/pmd/main.html
-#open build/reports/pmd/main.html
-#
-#xdg-open build/reports/pmd/test.html
-#open build/reports/pmd/test.html
-#
-#xdg-open build/reports/tests/test/index.html
-#open build/reports/tests/test/index.html
-#
-#xdg-open build/reports/jacoco/test/html/index.html
-#open build/reports/jacoco/test/html/index.html
-
+# Kill app
+pkill -f "node test-app"
