b51bd16c61b534683ce1b64d6ab637797b0d62e0
Louis Houghton <louis@louishoughton.com>
2018-03-27 14:59:20 +0100

    More output from test server

--- bin/test/test-app.js
+++ bin/test/test-app.js
@@ -12,5 +12,6 @@ app.use('/demproxy/dm/documents/1234-1234-1234/binary', express.static(__dirname
 const port = process.env.PORT || "7654";
 const server = http.createServer(app);
 app.set("port", port);
+console.log(`UP on port ${port}`);
 server.listen(port);
 
